92879b667c4c8d4daf73765d95dbb90c78da8bb5
ieb2 <ieb2@students.uwf.edu>
2022-09-26 18:41:36 -0500

    Reconsider a combination of drop-d-jackknife. Estimate is VERY non-smooth.

--- prototyp_script_for_new_algorithms.R
+++ prototyp_script_for_new_algorithms.R
@@ -15,7 +15,7 @@ contains_na <- map_lgl(subsamples, ~any(is.na(.x)))
 
 for(i in 1:length(subsamples)){
   if(contains_na[[i]] == TRUE){
-    subsamples[[i]] <- mice(subsamples[[i]], seed = 123, m = 2, method = "norm.nob", 
+    subsamples[[i]] <- mice(subsamples[[i]], seed = 123, m = 1, method = "pmm", 
                             print=FALSE, maxit = 1, predictorMatrix = uncon_pred_mat)
   } else{
     subsamples[[i]] <- subsamples[[i]]
@@ -56,19 +56,11 @@ for(i in 1:length(subsamples)){
 }
 
 # Jackknife point estimate
-jittered_analysis_vector <- jitter(analysis_vector, factor = 10000)
+jittered_analysis_vector <- jitter(analysis_vector, factor = 1000)
 point_estimate_jackknife <- mean(jittered_analysis_vector)
 
 density(jittered_analysis_vector) %>%
   plot()
-
-analysis_vector_split <- 
-  split(jittered_analysis_vector, 
-        cut(jittered_analysis_vector, 
-            quantile(jittered_analysis_vector, prob = c(0, 0.25, 0.50, 0.75, 1), names = FALSE), include = TRUE)) %>%
-  map(., ~mean(.x)) %>%
-  unlist() %>%
-  weighted.mean(., w=c(0.7, 0.1, 0.1, 0.1))
 
 # Quantile CI
 UB <- quantile(jittered_analysis_vector, 0.975)
@@ -102,12 +94,17 @@ prior_imp_V1 <- coef(sim_from_prior_imp) %>%
   unlist() %>% 
   as.vector()
 
-ks.test(x = analysis_vector, y = prior_analysis_V1)
-ks.test(x = analysis_vector, y = prior_imp_V1)
+mean_dist <- c(prior_imp_V1 , prior_analysis_V1, analysis_vector)
+as.data.frame(mean_dist) -> q
 
-plot(ecdf(analysis_vector), verticals=TRUE, do.points=FALSE, col="blue") 
-plot(ecdf(prior_analysis_V1), verticals=TRUE, do.points=FALSE, col="green", add=TRUE) 
-  
+data.frame(prior_imp_V1, prior_analysis_V1, analysis_vector) %>%
+  reshape2::melt() %>%
+  ggplot(., aes(x = value, fill = variable)) + 
+  geom_density() + 
+  geom_density(aes(mean_dist, fill = "mean_dist"), data = q) +
+  theme_classic() + 
+  expand_limits(y=c(0, 1))
+
 View(sim_from_prior_analysis)
 
 mean.fun <- function(d, i) {
@@ -144,3 +141,11 @@ boot.ci(reg_res, type="perc")
 
 # Try to come up with a way to justify the amount of noise added. 
 combn(analysis_vector, 2)
+
+analysis_vector_split <- 
+  split(jittered_analysis_vector, 
+        cut(jittered_analysis_vector, 
+            quantile(jittered_analysis_vector, prob = c(0, 0.25, 0.50, 0.75, 1), names = FALSE), include = TRUE)) %>%
+  map(., ~mean(.x)) %>%
+  unlist() %>%
+  weighted.mean(., w=c(0.7, 0.1, 0.1, 0.1))
