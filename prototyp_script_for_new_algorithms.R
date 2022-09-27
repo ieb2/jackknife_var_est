600d07e6e8de2334867ab2df38b216c53b84675b
Ihsan Buker <ieb2@students.uwf.edu>
2022-09-27 17:22:51 -0500

    Continue on bayesian analysis, moved bootstrap stuff from ptototype to scratch

--- prototyp_script_for_new_algorithms.R
+++ prototyp_script_for_new_algorithms.R
@@ -101,91 +101,20 @@ sim_from_prior_imp <-
   unlist() %>% 
   as.vector()
 
-data.frame(sim_from_prior_imp , sim_from_prior_analysis, analysis_vector) %>%
-  reshape2::melt() %>%
-  ggplot(., aes(x = value, fill = variable)) + 
-  geom_density() + 
-  #geom_density(aes(mean_dist, fill = "mean_dist"), data = q) +
-  theme_classic() + 
-  expand_limits(y=c(0, 1))
+sample_betas <- sim_from_prior_imp
+# Assuming betas come from normal dist with following parameters. 
+rnorm(n = length(sample_betas), mean =  mean(sample_betas), sd = sd(sample_betas))
+like <- dnorm(x = analysis_vector, mean = mean(sample_betas), sd = sd(sample_betas)) 
 
-dens_prior_analysis <- density(sim_from_prior_analysis)
-dens_prior_imputation <- density(sim_from_prior_imp)
-dens_analysis_vector <- density(analysis_vector)
+par(mfrow = c( 1,2 ) ) 
+beta_distr <- rnorm(n = 100000, mean(sample_betas), sd = sd(sample_betas))
+p1 <- hist(beta_distr, 60, col = "lightgreen")
 
-res <- 1 / length(analysis_vector) 
-beta.h <- seq(-3, 3, res)
-density <- dens_prior_analysis$y # Get the density for each value in mu.h
-prior <- density * res      # Multiply with resolution to get probability per interval
+possible_betas <- seq(-3, 3, by = 0.01)
 
-likelihood <- prior * analysis_vector
+likelihood <- dnorm(possible_betas, mean(sample_betas), sd = sd(sample_betas))
 
-raw_posterior <- lik * prior 
-
-standardized_posterior <- raw_posterior/sum(raw_posterior)
-
-rangeP <- seq(0, 1, length.out = length(lik))
-
-plot(rangeP, lik,
-     type = "l", xlab = "P(Black)", ylab = "Density", ylim = c(0,30))
-lines(rangeP, prior,
-      col = "red")
-lines(rangeP, raw_posterior, col = "green")
-lines(rangeP, standardized_posterior, col = "blue")
-legend("topleft", legend = c("Lik", "Prior", "Unstd Post", "Post"),
-       text.col = 1:4, bty = "n") 
-
-
-
-pp <- data.frame(density(sim_from_prior_imp)$y, density(sim_from_prior_analysis)$y,
-                 density(analysis_vector)$y) %>%
-  reshape2::melt() %>%
-  ggplot(., aes(x = value, fill = variable)) + 
-  geom_density() + 
-  geom_density(aes(x = values, fill = "standardized_posterior"),
-               data.frame(values = standardized_posterior)) 
-
-  plotly::ggplotly(pp)
-
-mean.fun <- function(d, i) {
-  m = mean(d[i])
-  n = length(d)
-  v = (n-1)*var(d[i])/n^2
-  c(m, v) }
-
-meanfun <- function(data, i){
-  d <- data[i]
-  return(mean(d))   
-}
-
-results <- boot(data=jittered_analysis_vector, statistic = mean.fun, R=1e4)
-
-plot(results)
-
-# get 95% confidence interval
-boot.ci(results, type="stud")
-
-regfun <- function(data, i){
-  d <- data[i, ]
-  mod <- lm(formula = outcome_variable ~ V1 + V2 + V3, data = d)
-    broom::tidy(mod) %>%
-    as.data.frame() %>%
-    dplyr::filter(term == "V1") %>%
-    dplyr::select(estimate) %>%
-    unlist()
-}
-
-reg_res <- boot(data = df_w_mis, statistic = regfun, R=1e3)
-plot(reg_res)
-boot.ci(reg_res, type="perc")
-
-# Try to come up with a way to justify the amount of noise added. 
-combn(analysis_vector, 2)
-
-analysis_vector_split <- 
-  split(jittered_analysis_vector, 
-        cut(jittered_analysis_vector, 
-            quantile(jittered_analysis_vector, prob = c(0, 0.25, 0.50, 0.75, 1), names = FALSE), include = TRUE)) %>%
-  map(., ~mean(.x)) %>%
-  unlist() %>%
-  weighted.mean(., w=c(0.7, 0.1, 0.1, 0.1))
+p2 <- plot( x = possible_betas,
+            y = likelihood,
+            type = 'h', 
+            xlim=c(1,3))
--- scratch_code.R
+++ scratch_code.R
@@ -26,3 +26,110 @@ for(i in 1:length(imps)) {
 
 
 mean(unlist(analysis_vector))
+
+data.frame(sim_from_prior_imp , sim_from_prior_analysis, analysis_vector) %>%
+  reshape2::melt() %>%
+  ggplot(., aes(x = value, fill = variable)) + 
+  geom_density() + 
+  #geom_density(aes(mean_dist, fill = "mean_dist"), data = q) +
+  theme_classic() + 
+  expand_limits(y=c(0, 1))
+
+dens_prior_analysis <- density(sim_from_prior_analysis)
+dens_prior_imputation <- density(sim_from_prior_imp)
+dens_analysis_vector <- density(analysis_vector)
+
+mu <- seq( 0, 10, by = 0.5 )
+sigma <- seq( 0.1, 10, by = 0.3 )
+pars <- expand.grid( mu = mu, sigma = sigma )
+
+plot( pars, pch = 19, main = 'The Parameter Space' )
+
+pars$mu_prior <- dnorm( pars$mu, mean = 18, sd = 5 )
+pars$sigma_prior <- dunif( pars$sigma, min = 0, max = 10 )
+pars$prior <- pars$mu_prior * pars$sigma_prior
+for( i in 1:nrow( pars ) ) {
+  likelihoods <- dnorm( temp, pars$mu[i], pars$sigma[i])
+  pars$likelihood[i] <- prod( likelihoods )
+}
+
+#calculate the posterior probability
+pars$probability <- pars$likelihood * pars$prior
+pars$probability <- pars$probability / sum( pars$probability )
+
+
+
+
+
+
+
+
+
+res <- seq(0, 1, length.out = length(analysis_vector)) 
+
+plot(rangeP, dens_prior_imputation,
+     type = "l", xlab = "P(Black)", ylab = "Density")
+
+plot(rangeP, lik,
+     type = "l", xlab = "P(Black)", ylab = "Density", ylim = c(0,30))
+lines(rangeP, prior,
+      col = "red")
+lines(rangeP, raw_posterior, col = "green")
+lines(rangeP, standardized_posterior, col = "blue")
+legend("topleft", legend = c("Lik", "Prior", "Unstd Post", "Post"),
+       text.col = 1:4, bty = "n") 
+
+
+
+pp <- data.frame(density(sim_from_prior_imp)$y, density(sim_from_prior_analysis)$y,
+                 density(analysis_vector)$y) %>%
+  reshape2::melt() %>%
+  ggplot(., aes(x = value, fill = variable)) + 
+  geom_density() + 
+  geom_density(aes(x = values, fill = "standardized_posterior"),
+               data.frame(values = standardized_posterior)) 
+
+plotly::ggplotly(pp)
+
+mean.fun <- function(d, i) {
+  m = mean(d[i])
+  n = length(d)
+  v = (n-1)*var(d[i])/n^2
+  c(m, v) }
+
+meanfun <- function(data, i){
+  d <- data[i]
+  return(mean(d))   
+}
+
+results <- boot(data=jittered_analysis_vector, statistic = mean.fun, R=1e4)
+
+plot(results)
+
+# get 95% confidence interval
+boot.ci(results, type="stud")
+
+regfun <- function(data, i){
+  d <- data[i, ]
+  mod <- lm(formula = outcome_variable ~ V1 + V2 + V3, data = d)
+  broom::tidy(mod) %>%
+    as.data.frame() %>%
+    dplyr::filter(term == "V1") %>%
+    dplyr::select(estimate) %>%
+    unlist()
+}
+
+reg_res <- boot(data = df_w_mis, statistic = regfun, R=1e3)
+plot(reg_res)
+boot.ci(reg_res, type="perc")
+
+# Try to come up with a way to justify the amount of noise added. 
+combn(analysis_vector, 2)
+
+analysis_vector_split <- 
+  split(jittered_analysis_vector, 
+        cut(jittered_analysis_vector, 
+            quantile(jittered_analysis_vector, prob = c(0, 0.25, 0.50, 0.75, 1), names = FALSE), include = TRUE)) %>%
+  map(., ~mean(.x)) %>%
+  unlist() %>%
+  weighted.mean(., w=c(0.7, 0.1, 0.1, 0.1))
