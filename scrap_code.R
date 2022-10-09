1ee7cd42b0a41f9e0e1fa9c18d0fa5aeba1ea334
Ihsan Buker <ieb2@students.uwf.edu>
2022-10-08 20:38:32 -0500

    NA

--- scrap_code.R
+++ scrap_code.R
@@ -1,6 +1,6 @@
 n_sub_samples <- 1e3
 
-n = nrow(df_w_mis); k = 40
+n = nrow(df_w_mis); k = 10
 
 length_sample_vec <- choose(n, k)
 
@@ -32,6 +32,8 @@ pseudo_estimates <- vapply(imputations, function(x) .lm.fit(x = x[,1:3],
                                                             y = x[,4])$coefficients[1], numeric(1))
 
 jackknife_point_estimate <- mean.default(pseudo_estimates)
+
+density(pseudo_estimates) %>% plot()
 
 LB <- quantile(pseudo_estimates, 0.025)
 UB <- quantile(pseudo_estimates, 0.975)
