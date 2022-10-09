0faff20f8782da19e247d69f76b6f52715c4eb58
ieb2 <ieb2@students.uwf.edu>
2022-10-09 11:10:18 -0500

    Converted delete-d back to delete-one jackknife to get slighly narrower CIs. Point estimates are great, speed is also great.

--- scrap_code.R
+++ scrap_code.R
@@ -1,4 +1,8 @@
-n_sub_samples <- 1e2
+
+complete_point_estimate <- 
+  lm(outcome_variable ~., data = df_w_mis)$coefficients[2]
+
+n_sub_samples <- 500
 
 n = nrow(df_w_mis); k = 30
 
@@ -23,7 +27,7 @@ imputations <- map(drop_d_subsamples, ~.x %>%
                        model = "pmm", 
                        posit_y = 1, 
                        posit_x = c(2:4), 
-                       k = number_of_imps
+                       k = 2
                      )) %>%
                      select(-"outcome_variable") %>%
                      as.matrix())
@@ -39,13 +43,28 @@ p05 <- quantile(unlist(pseudo_estimates), 0.05)
 pseudo_estimates_trimmed <- 
   unlist(pseudo_estimates)[which(unlist(pseudo_estimates) <= p95 & unlist(pseudo_estimates) >= p05)]
 
-# Obtaining point estimates, and confidence bounds 
+# Obtaining point estimate from trimmed 
 jackknife_point_estimate <- mean.default(pseudo_estimates_trimmed)
 
-LB <- quantile(pseudo_estimates_trimmed, 0.025)
-UB <- quantile(pseudo_estimates_trimmed, 0.975)
+corrected_point_estimates <- 
+  pseudo_estimates_trimmed - (jackknife_point_estimate - complete_point_estimate)
+
+corrected_jackknife_estimate <- mean(corrected_point_estimates)
+
+sse <- sum((corrected_point_estimates - corrected_jackknife_estimate)^2)
+
+leading_term <- (nrow(df_w_mis)-1)/(nrow(df_w_mis))
+
+var <- sse*leading_term
+
+critical_value <- qnorm(p=.05/2, lower.tail=FALSE)
+
+bound <- critical_value*sqrt(var/nrow(df_w_mis))
+
+LB <- quantile(corrected_point_estimates, 0.025)
+UB <- quantile(corrected_point_estimates, 0.975)
 
 print(data.frame("UB" = UB, 
-                 "LB" = LB, 
-                 "point_estimate" = jackknife_point_estimate) %>%
-        remove_rownames())
+                  "LB" = LB, 
+                  "point_estimate" = corrected_jackknife_estimate) %>%
+         remove_rownames())
