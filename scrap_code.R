033453970a45695ec31ec4d42dea105c8a678738
ieb2 <ieb2@students.uwf.edu>
2022-10-08 22:12:57 -0500

    Decent coverage but bad point estimates

--- scrap_code.R
+++ scrap_code.R
@@ -1,6 +1,6 @@
-n_sub_samples <- 1e3
+n_sub_samples <- 1e2
 
-n = nrow(df_w_mis); k = 10
+n = nrow(df_w_mis); k = 30
 
 length_sample_vec <- choose(n, k)
 
@@ -23,22 +23,29 @@ imputations <- map(drop_d_subsamples, ~.x %>%
                        model = "pmm", 
                        posit_y = 1, 
                        posit_x = c(2:4), 
-                       k = 10
+                       k = number_of_imps
                      )) %>%
                      select(-"outcome_variable") %>%
                      as.matrix())
 
-pseudo_estimates <- vapply(imputations, function(x) .lm.fit(x = x[,1:3], 
-                                                            y = x[,4])$coefficients[1], numeric(1))
+# subset_imp <- map(1:length(imputations), ~imputations[-.x])
 
-jackknife_point_estimate <- mean.default(pseudo_estimates)
+pseudo_estimates <- 
+  vapply(imputations, function(x) .lm.fit(x = x[,1:3], y = x[,4])$coefficients[1], numeric(1))
 
-density(pseudo_estimates) %>% plot()
+# Trimming the pseudo_estimate_vector 
+p95 <- quantile(unlist(pseudo_estimates), 0.95)
+p05 <- quantile(unlist(pseudo_estimates), 0.05)
+pseudo_estimates_trimmed <- 
+  unlist(pseudo_estimates)[which(unlist(pseudo_estimates) <= p95 & unlist(pseudo_estimates) >= p05)]
 
-LB <- quantile(pseudo_estimates, 0.025)
-UB <- quantile(pseudo_estimates, 0.975)
+# Obtaining point estimates, and confidence bounds 
+jackknife_point_estimate <- mean.default(pseudo_estimates_trimmed)
+
+LB <- quantile(pseudo_estimates_trimmed, 0.025)
+UB <- quantile(pseudo_estimates_trimmed, 0.975)
 
 print(data.frame("UB" = UB, 
-                  "LB" = LB, 
-                  "point_estimate" = jackknife_point_estimate) %>%
-         remove_rownames())
+                 "LB" = LB, 
+                 "point_estimate" = jackknife_point_estimate) %>%
+        remove_rownames())
